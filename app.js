8076a5d487626e01bd4750a0c6705a2e9378c4fd
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-08-27 21:20:13 -0400

    Add app.js file

---
+++ app.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+//multer
+//cloudinary
+//streamifier
+
+//expressSession
+//passport
+// require("./middleware/passport");
+// const passport = require("passport");
+// const LocalStrategy = require("passport-local").Strategy;
+//jwt
+
+const app = express();
+app.use(cors());
+
+// Passport to routes
+// app.use(passport.initialize());
+
+// Parse form payloads and sets to req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routers
+// const indexRouter = require("./routes/index");
+// const userRouter = require("./routes/user");
+
+
+// app.use("/", indexRouter);
+// app.use("/user", userRouter);
+
+
+// Fallback error route
+app.get(/.*/, (req, res, next) => {
+  res.send("Not a route.");
+});
+
+// const PORT = 3000;
+
+// app.listen(PORT, "::", () => {
+//   console.log(`Listening on port ${PORT}.`);
+// });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Listening on port ${PORT}.`);
+});
