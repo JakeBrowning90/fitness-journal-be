406ece08a31b896c5d1f4a49d79de0ff8c82e4af
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-10-07 14:24:39 -0400

    Add basic validation to session submit function

--- middleware/validateSession.js
+++ middleware/validateSession.js
@@ -2,7 +2,14 @@ const { body } = require("express-validator");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const validateSessionCreate = [];
+const validateSessionCreate = [
+  body("user").notEmpty().withMessage("User required"),
+  body("date").notEmpty().withMessage("Date selection required"),
+  body("exercise").notEmpty().withMessage("Exercise selection required"),
+  body("duration").optional(),
+  body("distance").optional(),
+  body("notes").optional(),
+];
 
 const validateSessionUpdate = [];
 
