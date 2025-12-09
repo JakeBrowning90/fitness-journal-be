cabf57a198fcb6c90fec8a421f774bd27498d1e8
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-12-09 15:34:31 -0500

    Add token check middleware

---
+++ middleware/verifyToken.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+verifyToken = (req, res, next) => {
+  //Confirmation status check
+  const confirmStatus = req.headers["confirmation"];
+  if (confirmStatus == "false") {
+    res.sendStatus(401).json("Unauthorized");
+  }
+  // get auth header value
+  const bearerHeader = req.headers["authorization"];
+  // Separate token from 'Bearer ' header
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+
+    try {
+      jwt.verify(req.token, process.env.SECRET_KEY);
+    } catch (err) {
+      res.sendStatus(401);
+    }
+
+    next();
+  } else {
+    res.sendStatus(401).json("Unauthorized");
+  }
+};
+
+module.exports = verifyToken;
